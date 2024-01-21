10b5b75f4ce237afe9035a41371bdbe3652212c8
TENACIOUS711 <105707227+TENACIOUS711@users.noreply.github.com>
2024-01-22 03:24:08 +0530

    winning highlight

--- OneDrive/Desktop/REACT projects/tictactoe-vite/src/components/square.jsx
+++ OneDrive/Desktop/REACT projects/tictactoe-vite/src/components/square.jsx
@@ -1,6 +1,12 @@
-const Square = ({ value, onClick }) => {
+const Square = ({ value, onClick, isWinningSquare }) => {
+  const textColor = value === 'X' ? 'text-green' : 'text-orange';
+  const winSquare = isWinningSquare ? 'winning' : '';
   return (
-    <button type="button" className="square" onClick={onClick}>
+    <button
+      type="button"
+      className={`square ${textColor} ${winSquare}`}
+      onClick={onClick}
+    >
       {value}
     </button>
   );
