5a614fb3766040074a44cf98ed1968d27e0bd531
TENACIOUS711 <105707227+TENACIOUS711@users.noreply.github.com>
2024-01-22 04:47:41 +0530

    bg-balls

--- OneDrive/Desktop/REACT projects/tictactoe-vite/src/App.jsx
+++ OneDrive/Desktop/REACT projects/tictactoe-vite/src/App.jsx
@@ -83,6 +83,7 @@ function App() {
         RESET GAME
       </button>
       <History history={history} moveTo={moveTo} currentMove={currentMove} />
+      <div className="bg-balls"></div>
     </div>
   );
 }
